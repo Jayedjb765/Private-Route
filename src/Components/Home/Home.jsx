835095b825c3919b854275f7d9615898f8722a44
Jayedjb765 <jayedjb76@gmail.com>
2024-08-21 12:30:58 +0600

    Adding Navbar

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <he className="text-3xl">This is home page</he>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+import Login from './Components/Login/Login.jsx';
+import Register from './Components/Register/Register.jsx';
+import Food from './Components/Food/Food.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children : [
+      {
+        path:'/log',
+        element:<Login></Login>
+      },
+      {
+        path:'/reg',
+        element: <Register></Register>,
+
+      },
+      {
+        path: "/food",
+        element: <Food></Food>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router} />
+  </StrictMode>,
+)
