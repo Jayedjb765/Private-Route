a742b488da6cc99412e1ee88ee7cbc84308e829c
Jayedjb765 <jayedjb76@gmail.com>
2024-08-21 12:55:49 +0600

    Authprovider adding

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,25 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useState } from "react";
+import auth from "../firebase/firebase.init";
 
 export const AuthContext = createContext(null);
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
+    const [user,useuser] =  useState(null);
+    const [loading,setloading] =  useState(true);
+    const createuser  = (email,password) =>{
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const signinuser =  (email,password) =>{
+        return signInWithEmailAndPassword(auth,email,password )
+    }
+    const loogout = () =>{
+        return signOut(auth)
+    }
+    const authinform = {user,loading,createuser,signinuser,loogout};
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authinform}>
+                {children}
+        </AuthContext.Provider>
     );
 };
 
