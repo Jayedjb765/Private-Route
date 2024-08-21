e6e92934a4f3aaa2d8667a9400b6ad16556c9fbd
Jayedjb765 <jayedjb76@gmail.com>
2024-08-21 13:06:28 +0600

    Updating auth

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,20 +1,33 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.init";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({children}) => {
-    const [user,useuser] =  useState(null);
+    const [user,setuser] =  useState(null);
     const [loading,setloading] =  useState(true);
     const createuser  = (email,password) =>{
+        setloading (true);
         return createUserWithEmailAndPassword(auth,email,password);
     }
     const signinuser =  (email,password) =>{
+        setloading (true);
         return signInWithEmailAndPassword(auth,email,password )
     }
     const loogout = () =>{
+        setloading (true);
         return signOut(auth)
     }
+    useEffect(()=>{
+        const unvisible = onAuthStateChanged(auth,currentuser=>{
+            setuser(currentuser);
+            setloading (false);
+        })
+        return () =>{
+            unvisible();
+
+        }
+    },[])
     const authinform = {user,loading,createuser,signinuser,loogout};
     return (
         <AuthContext.Provider value={authinform}>
